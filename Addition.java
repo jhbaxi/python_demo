8fee747cfefcea7bf5c7f46393864b7922cd2eb7
JHB <baxi099@gmail.com>
2025-03-10 12:04:23 +0530

    second commit

---
+++ Addition.java
@@ -0,0 +1,22 @@
+
+
+public class Addition {
+    public static void main(String[] args) {
+   
+
+        // Input two numbers
+      
+        int num1 = 5;
+
+      
+        int num2 = 10;
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+       
+    }
+}
